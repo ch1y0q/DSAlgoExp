8d39916286dc5e84318362a62ccb7368b1e9c3da
qwerty250 <qiyue2001@gmail.com>
2021-11-06 23:07:23 +0800

    Add buffer window; operator[] returns r-value; Format

--- simulator.cpp
+++ simulator.cpp
@@ -1,98 +1,91 @@
 /**
  * @file simulator.cpp
  * @author HUANG Qiyue
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-11-05
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
-#include <iostream>
-#include <fstream>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <random>
-#include "structures.h"
-#include "defs.h"
 
-matrix_t<uint32_t>
-generate_matrix(uint32_t r, uint32_t c)
-{
+#include "defs.h"
+#include "structures.h"
+
+matrix_t<uint32_t> generate_matrix(uint32_t r, uint32_t c) {
     matrix_t<uint32_t> m32;
 
     /* metadata */
-    m32.magic=MAGIC;
-    m32.size_of_T=sizeof(uint32_t);
-    m32.row=r;
-    m32.col=c;
+    m32.magic = MAGIC;
+    m32.size_of_T = sizeof(uint32_t);
+    m32.row = r;
+    m32.col = c;
 
     auto arr = new uint32_t*[r];
     /* generate random number */
     std::mt19937 rng(std::random_device{}());
-    for(auto i=0; i<r; ++i){
-        arr[i]=new uint32_t[c];
-        for(auto j=0;j<c;++j){
-            arr[i][j]=rng();
+    for (auto i = 0; i < r; ++i) {
+        arr[i] = new uint32_t[c];
+        for (auto j = 0; j < c; ++j) {
+            arr[i][j] = rng();
         }
     }
 
-    m32.arr=arr;
+    m32.arr = arr;
     return m32;
 }
 
 template <class T>
-void
-dump_matrix(matrix_t<T>& mat, const char* filename)
-{
+void dump_matrix(matrix_t<T>& mat, const char* filename) {
     /* Create file object and open file */
     std::fstream fs(filename, std::ios::out | std::ios::binary);
-    if (!fs)
-    {
+    if (!fs) {
         std::cerr << "Error opening file. Aborting.\n";
         return;
     }
     /* dump metadata into file */
-    fs.write(reinterpret_cast<char *>(&mat), MATRIX_METADATA * sizeof(uint32_t));
+    fs.write(reinterpret_cast<char*>(&mat), MATRIX_METADATA * sizeof(uint32_t));
 
     /* dump array data into file */
-    for(auto i=0; i<mat.row; ++i){
-        for(auto j=0;j<mat.col;++j){
-            fs.write(reinterpret_cast<char *>(&(mat.arr[i][j])), sizeof(T));
+    for (auto i = 0; i < mat.row; ++i) {
+        for (auto j = 0; j < mat.col; ++j) {
+            fs.write(reinterpret_cast<char*>(&(mat.arr[i][j])), sizeof(T));
         }
     }
     fs.close();
 }
 
 template <class T>
-matrix_t<T>
-load_matrix(const char* filename)
-{
-    matrix_t<T> mat={0};
+matrix_t<T> load_matrix(const char* filename) {
+    matrix_t<T> mat = {0};
 
     /* Create file object and open file */
     std::fstream fs(filename, std::ios::in | std::ios::binary);
-    if (!fs)
-    {
+    if (!fs) {
         std::cerr << "Error opening file. Aborting.\n";
         return mat;
     }
     /* read metadata from file */
-    fs.read((char*) &mat.magic, sizeof(uint32_t));
-    fs.read((char*) &mat.row, sizeof(uint32_t));
-    fs.read((char*) &mat.col, sizeof(uint32_t));
-    fs.read((char*) &mat.size_of_T, sizeof(uint32_t));
+    fs.read((char*)&mat.magic, sizeof(uint32_t));
+    fs.read((char*)&mat.row, sizeof(uint32_t));
+    fs.read((char*)&mat.col, sizeof(uint32_t));
+    fs.read((char*)&mat.size_of_T, sizeof(uint32_t));
 
     /* initialize array */
     mat.arr = new uint32_t*[mat.row];
-    for(auto i=0; i<mat.row; ++i){
-        mat.arr[i]=new uint32_t[mat.col];
+    for (auto i = 0; i < mat.row; ++i) {
+        mat.arr[i] = new uint32_t[mat.col];
     }
 
     /* read array data from file */
-    for(auto i=0; i<mat.row; ++i){
-        for(auto j=0;j<mat.col;++j){
-            fs.read((char*) &mat.arr[i][j], sizeof(T));
+    for (auto i = 0; i < mat.row; ++i) {
+        for (auto j = 0; j < mat.col; ++j) {
+            fs.read((char*)&mat.arr[i][j], sizeof(T));
         }
     }
 
@@ -100,13 +93,24 @@ load_matrix(const char* filename)
     return mat;
 }
 
-#define LOOP(_a,_b,_c) for(size_t _a = 0; _a < N; ++_a){for(size_t _b = 0; _b < N; ++_b){for(size_t _c = 0; _c < N; ++_c){c.assign(i,k,c[i][k]+a[i][j]*b[j][k]);}}}
-#define SHOW_STAT(_x) printf("==" #_x "==\nRead: %d, Miss: %d, Miss Ratio: %.3f\n", _x.read_count, _x.miss_count, _x.miss_rate());
+#define LOOP(_a, _b, _c)                                     \
+    for (size_t _a = 0; _a < N; ++_a) {                      \
+        for (size_t _b = 0; _b < N; ++_b) {                  \
+            for (size_t _c = 0; _c < N; ++_c) {              \
+                c.assign(i, k, c[i][k] + a[i][j] * b[j][k]); \
+            }                                                \
+        }                                                    \
+    }
+#define SHOW_STAT(_x)                                             \
+    printf("==" #_x "==\nRead: %d, Miss: %d, Miss Ratio: %.3f\n", \
+           _x.read_count, _x.miss_count, _x.miss_rate());
 #define SHOW_STATS SHOW_STAT(a) SHOW_STAT(b) SHOW_STAT(c)
-#define RUN_SIM(_a, _b, _c) printf(#_a #_b #_c ":\n");LOOP(_a,_b, _c);SHOW_STATS;printf("\n\n");
-void
-run_simulation(size_t N, size_t capacity)
-{
+#define RUN_SIM(_a, _b, _c)    \
+    printf(#_a #_b #_c ":\n"); \
+    LOOP(_a, _b, _c);          \
+    SHOW_STATS;                \
+    printf("\n\n");
+void run_simulation(size_t N, size_t capacity, size_t window) {
     auto ma = generate_matrix(N, N);
     auto mb = generate_matrix(N, N);
     auto mc = generate_matrix(N, N);
@@ -114,33 +118,29 @@ run_simulation(size_t N, size_t capacity)
     dump_matrix(mb, "b.dat");
     dump_matrix(mc, "c.dat");
 
-    buffer_t<uint32_t> a("a.dat", capacity);
-    buffer_t<uint32_t> b("b.dat", capacity);
-    buffer_t<uint32_t> c("c.dat", capacity);
+    buffer_t<uint32_t> a("a.dat", capacity, window);
+    buffer_t<uint32_t> b("b.dat", capacity, window);
+    buffer_t<uint32_t> c("c.dat", capacity, window);
 
-    RUN_SIM(i,j,k);
-    RUN_SIM(i,k,j);
-    RUN_SIM(j,i,k);
-    RUN_SIM(j,k,i);
-    RUN_SIM(k,i,j);
-    RUN_SIM(k,j,i);
+    RUN_SIM(i, j, k);
+    RUN_SIM(i, k, j);
+    RUN_SIM(j, i, k);
+    RUN_SIM(j, k, i);
+    RUN_SIM(k, i, j);
+    RUN_SIM(k, j, i);
 }
 
 template <class T>
-void
-free_matrix(matrix_t<T>& mat)
-{
-    for(auto i=0; i<mat.row; ++i){
+void free_matrix(matrix_t<T>& mat) {
+    for (auto i = 0; i < mat.row; ++i) {
         delete[] mat.arr[i];
     }
 
     delete[] mat.arr;
 }
 
-int
-main()
-{
-    run_simulation(5,10);
-    std::cout<<"done\n";
+int main() {
+    run_simulation(5, 10, 3);
+    std::cout << "done\n";
     return 0;
 }
--- structures.h
+++ structures.h
@@ -1,31 +1,30 @@
 /**
  * @file structures.h
  * @author HUANG Qiyue
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-11-05
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
-#include <iostream>
-#include <unordered_map>
-#include <list>
 #include <fstream>
+#include <iostream>
+#include <list>
+#include <unordered_map>
 
 #include "defs.h"
 /**
  * @brief Defines a generic 2-D matrix with metadata.
- * 
+ *
  * @tparam T Type of variables stored in the matrix.
  */
 template <class T>
-struct matrix_t
-{
+struct matrix_t {
     /* metadata */
     /* MATRIX_METADATA in defs.h */
     uint32_t magic;
@@ -34,7 +33,7 @@ struct matrix_t
     uint32_t size_of_T;
 
     /* 2-D array */
-    T** arr;
+    T **arr;
 };
 
 /**
@@ -42,37 +41,35 @@ struct matrix_t
  *        LRU as replacement algorithm,
  *        write-through as write policy,
  *        and records buffer miss count against total read count.
- * 
+ *
  * @tparam T Type of variables stored in the matrix.
  */
 template <class T>
-class buffer_t
-{
-private:
+class buffer_t {
+   private:
     struct vector_t;
     struct cache_line;
-public:
+
+   public:
     /* constructors */
-    buffer_t(char const* filename, size_t _size)
-    {
+    buffer_t(char const *filename, size_t _size, size_t _window = 1) {
         fs.open(filename, std::ios::in | std::ios::out | std::ios::binary);
         capacity = _size;
+        window = _window;
         read_count = 0;
         miss_count = 0;
 
         /* load metadata */
-        //fs.read((char*) &magic, sizeof(uint32_t));
-        fs.seekg(sizeof(uint32_t), std::ios::beg);/* magic */
-        fs.read((char*) &row, sizeof(uint32_t));
-        fs.read((char*) &col, sizeof(uint32_t));
-        fs.read((char*) &size_of_T, sizeof(uint32_t));
+        // fs.read((char*) &magic, sizeof(uint32_t));
+        fs.seekg(sizeof(uint32_t), std::ios::beg); /* magic */
+        fs.read((char *)&row, sizeof(uint32_t));
+        fs.read((char *)&col, sizeof(uint32_t));
+        fs.read((char *)&size_of_T, sizeof(uint32_t));
     }
 
     /* operators */
-    vector_t operator[](size_t i)
-    {
-        if( i >= row )
-        {
+    vector_t operator[](size_t i) {
+        if (i >= row) {
             std::cerr << "Index exceeds upper bound." << std::endl;
             return vector_t(0, this);
         }
@@ -80,64 +77,63 @@ public:
     }
 
     /* helpers */
-    void assign(size_t i, size_t j, T val)
-    {
-        auto key = i*(col)+j;
-        fs.seekp(MATRIX_ARR_OFFSET + key*sizeof(T), std::ios::beg);
+    void assign(size_t i, size_t j, T val) {
+        auto key = i * (col) + j;
+        fs.seekp(MATRIX_ARR_OFFSET + key * sizeof(T), std::ios::beg);
         fs.write(reinterpret_cast<char *>(&val), sizeof(T));
         LRU_get(i, j);
     }
 
-    T seekg_and_read(size_t arr_offset)
-    {
-        fs.seekg(MATRIX_ARR_OFFSET + arr_offset,std::ios::beg);
-        T ret; 
-        fs.read((char*) &ret, sizeof(T));
+    T seekg_and_read(size_t arr_offset) {
+        fs.seekg(MATRIX_ARR_OFFSET + arr_offset, std::ios::beg);
+        T ret;
+        fs.read((char *)&ret, sizeof(T));
         return ret;
     }
 
     /* LRU */
-    void LRU_set(size_t key, T value){
+    void LRU_set(size_t key, T value) {
         // key not found
-		if (cache_map.find(key) == cache_map.end())
-		{
-			// if is full
-			if (cache_list.size() == capacity)
-			{
-				cache_map.erase(cache_list.back().key);
-				cache_list.pop_back();
-			}
-			cache_list.push_front(cache_line(key,value));
-			cache_map[key] = cache_list.begin();
-		} 
-		else
-		{
-			// update and make it the first
-			cache_map[key]->value = value;
-			cache_list.splice(cache_list.begin(),cache_list,cache_map[key]);
-			cache_map[key] = cache_list.begin();
-		}
-	}
-
-    T LRU_get(size_t i, size_t j)
-    {
-        read_count++;
-        auto key = i*(col)+j;
-        if (cache_map.find(key) == cache_map.end()){   // cache miss!
-            miss_count++;
-            auto ret = seekg_and_read(key*sizeof(T));
-            LRU_set(key,ret);
-            return ret;
-        }
-        else{
-		    cache_list.splice(cache_list.begin(),cache_list,cache_map[key]);
-		    cache_map[key] = cache_list.begin();
-		    return cache_map[key]->value;
+        if (cache_map.find(key) == cache_map.end()) {
+            // if is full
+            if (cache_list.size() == capacity) {
+                cache_map.erase(cache_list.back().key);
+                cache_list.pop_back();
+            }
+            cache_list.push_front(cache_line(key, value));
+            cache_map[key] = cache_list.begin();
+        } else {
+            // update and make it the first
+            cache_map[key]->value = value;
+            cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
+            cache_map[key] = cache_list.begin();
         }
     }
-    
+
+    T LRU_get(size_t i, size_t j) {
+        read_count++;
+        auto key = i * (col) + j;
+        if (cache_map.find(key) == cache_map.end()) {  // cache miss!
+            miss_count++;
+            auto ret = seekg_and_read(key * sizeof(T));
+            LRU_set(key, ret);
+            for (auto k = 2; k <= window; ++k) {  // pre-fetch
+                key++;
+                if (key >= row * col) break;
+                auto v = seekg_and_read(key * sizeof(T));
+                LRU_set(key, v);
+            }
+            return ret;
+        } else {
+            cache_list.splice(cache_list.begin(), cache_list, cache_map[key]);
+            cache_map[key] = cache_list.begin();
+            return cache_map[key]->value;
+        }
+    }
+
     /* buffer params */
     size_t capacity;
+    size_t window;
 
     /* persistent */
     uint32_t row;
@@ -145,46 +141,45 @@ public:
     uint32_t size_of_T;
 
     /* volatile */
-	std::list<cache_line> cache_list;
-	std::unordered_map<size_t, typename std::list<cache_line>::iterator> cache_map;
+    std::list<cache_line> cache_list;
+    std::unordered_map<size_t, typename std::list<cache_line>::iterator>
+        cache_map;
 
     /* stat */
     uint32_t miss_count;
     uint32_t read_count;
-    inline float miss_rate() {return 1.0f * miss_count/read_count;}
-    inline void reset_counters() {miss_count = read_count = 0;}
-private:
+    inline float miss_rate() { return 1.0f * miss_count / read_count; }
+    inline void reset_counters() { miss_count = read_count = 0; }
+
+   private:
     std::fstream fs;
 
     /* structures */
-    struct cache_line{
-		uint32_t key;
-		T value;
-		cache_line(uint32_t k,T v):key(k),value(v){}
-	};
+    struct cache_line {
+        uint32_t key;
+        T value;
+        cache_line(uint32_t k, T v) : key(k), value(v) {}
+    };
 
-    struct vector_t{
+    struct vector_t {
         size_t i;
         buffer_t<T> *parent;
         /* constructors */
-        vector_t(size_t _i, buffer_t<T> *e) : i(_i), parent(e){}
+        vector_t(size_t _i, buffer_t<T> *e) : i(_i), parent(e) {}
 
         /* operators */
 
-        /* l-value */
-        T& operator[](size_t j)
-        {
-            T* ret= new T;  // TODO: delete!
+        /* r-value */
+        const T operator[](size_t j) const {
+            T ret;
 
-            if( j >= parent->col )
-            {
+            if (j >= parent->col) {
                 std::cerr << "Index exceeds upper bound." << std::endl;
-                *ret =  parent->seekg_and_read(i*(parent->col)*sizeof(T));
+                ret = parent->seekg_and_read(i * (parent->col) * sizeof(T));
+            } else {
+                ret = parent->LRU_get(i, j);
             }
-            else{
-                *ret = parent->LRU_get(i, j);
-            }
-            return *ret;
+            return ret;
         }
     };
 };
